5b12fd18fc4c513e35bbd7bb28f71dbc84137c15
Mathis <98806470+MathisFngl@users.noreply.github.com>
2022-11-08 16:44:26 +0100

    Cmake

---
+++ cmake-build-debug/CMakeFiles/projet_IF2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/projet_IF2.dir/main.c.obj"
+  "CMakeFiles/projet_IF2.dir/main.c.obj.d"
+  "libprojet_IF2.dll.a"
+  "projet_IF2.exe"
+  "projet_IF2.exe.manifest"
+  "projet_IF2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/projet_IF2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/projet_IF2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for projet_IF2.
+# This may be replaced when dependencies are built.
