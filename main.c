c553e0957fe03c7d1da3256e2b61b959d22b9c4c
Mathis <98806470+MathisFngl@users.noreply.github.com>
2022-11-09 07:53:12 +0100

    Hello World

--- main.c
+++ main.c
@@ -1,1 +1,6 @@
 #include <stdint.h>
+#include <stdio.h>
+
+void main(){
+    printf("Hello World");
+}
---
+++ main.h
@@ -0,0 +1,6 @@
+#ifndef PROJET_IF2_MAIN_H
+#define PROJET_IF2_MAIN_H
+
+#endif //PROJET_IF2_MAIN_H
+
+void main();
